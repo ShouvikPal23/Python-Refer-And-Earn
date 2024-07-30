7b9dbe11f7d6ce94fbb8fc176e1106ab13dcc3d6
Jnbots <158847629+Jnbots@users.noreply.github.com>
2024-07-30 22:52:39 +0530

    Update withdraw.py

--- Plugins/modules/withdraw.py
+++ Plugins/modules/withdraw.py
@@ -2,7 +2,7 @@ from .. import Mukesh
 from pyrogram import Client, enums, filters
 from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton, KeyboardButton, ReplyKeyboardMarkup
 from ..database import collection
-from config import OWNER_ID as owner_id, all_platform
+from config import OWNER_ID as owner_id, main_button
 import asyncio
 
 def decrease_balance(user_id, amount):
@@ -13,18 +13,18 @@ async def withdrawal(bot, message):
     document = collection.find_one({"user_id": message.from_user.id})
     balance = document.get("balance")
 
-    msg1 = await Mukesh.ask(message.from_user.id, "How much money do you want to withdraw?", reply_markup=all_platform)
+    msg1 = await Mukesh.ask(message.from_user.id, "How much money do you want to withdraw?", reply_markup=main_button)
     try:
         amount = float(msg1.text)
     except ValueError:
-        await message.reply_text("Invalid amount. Please enter a valid number.", reply_markup=all_platform)
+        await message.reply_text("Invalid amount. Please enter a valid number.", reply_markup=main_button)
         return
 
     if amount > balance:
-        await message.reply_text("Insufficient balance. Please enter a valid amount.", reply_markup=all_platform)
+        await message.reply_text("Insufficient balance. Please enter a valid amount.", reply_markup=main_button)
         return
 
-    msg2 = await Mukesh.ask(message.from_user.id, "Please provide your UPI ID.", reply_markup=all_platform)
+    msg2 = await Mukesh.ask(message.from_user.id, "Please provide your UPI ID.", reply_markup=main_button)
     upi_id = msg2.text
 
     # Decrease the user's balance
@@ -43,5 +43,5 @@ async def withdrawal(bot, message):
         ])
     )
 
-    await message.reply_text("Your withdrawal request has been sent for approval.", reply_markup=all_platform)
+    await message.reply_text("Your withdrawal request has been sent for approval.", reply_markup=main_button)
 
