d07ba96bde5dd5582bf17c1ae2e944797e602cc8
Jnbots <158847629+Jnbots@users.noreply.github.com>
2024-07-31 06:48:03 +0530

    Update add_balance.py

--- Plugins/modules/add_balance.py
+++ Plugins/modules/add_balance.py
@@ -1,17 +1,17 @@
-from .. import Mukesh
+from .. import JN
 from ..database import update_balance, deposits,collection
 from pyrogram import filters
 from config import OWNER_ID
 
-@Mukesh.on_message(filters.command("add"),group=7)
+@JN.on_message(filters.command("add"),group=7)
 async def handle_message(client, message):
     # print(message)
     if message.from_user.id==int(OWNER_ID):
     # if message.command[0].lower() == 'add':
-        msg1 = await Mukesh.ask(int(OWNER_ID), "How much money do you want to add? Please provide amount in INR format.")
+        msg1 = await JN.ask(int(OWNER_ID), "How much money do you want to add? Please provide amount in INR format.")
         amount = float(msg1.text)
         print(amount)
-        msg2 = await Mukesh.ask(int(OWNER_ID), "Please provide the user ID to whom you want to add money.")
+        msg2 = await JN.ask(int(OWNER_ID), "Please provide the user ID to whom you want to add money.")
         user_id = int(msg2.text)
         print(user_id)
         response = await deposits(user_id, amount)
