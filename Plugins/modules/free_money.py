ea35bb4529c3a0fefe054811fe9453d85708320c
Jnbots <158847629+Jnbots@users.noreply.github.com>
2024-07-31 06:48:46 +0530

    Update free_money.py

--- Plugins/modules/free_money.py
+++ Plugins/modules/free_money.py
@@ -1,6 +1,6 @@
 from pyrogram import filters, Client
 import asyncio
-from .. import Mukesh
+from .. import JN
 from pyrogram.enums import ParseMode
 from pyrogram.errors import UserNotParticipant, ChatWriteForbidden, ChatAdminRequired
 from config import UPDATE_CHNL, SUPPORT_GRP as Update2,START_IMG,all_platform
@@ -10,10 +10,10 @@ from config import UPDATE_CHNL, SUPPORT_GRP as Update2,START_IMG,all_platform
 from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
 
 
-@Mukesh.on_message(filters.regex('🤑 ꜰʀᴇᴇ ᴍᴏɴᴇʏ 🤑') & filters.private)
+@JN.on_message(filters.regex('🤑 ꜰʀᴇᴇ ᴍᴏɴᴇʏ 🤑') & filters.private)
 async def must_join_channel(bot: Client, msg):
     await msg.reply("**ᴘʟᴇᴀꜱᴇ ꜱᴇʟᴇᴄᴛ ᴏᴘᴛɪᴏɴ:**\n\nɪɴ ʀᴇꜰᴇʀ ᴀɴᴅ ᴇᴀʀɴ ʏᴏᴜ ᴄᴀɴ ᴇᴀʀɴ ʙʏ ɪɴᴠɪᴛᴇ ᴜꜱᴇʀ\n\nɪɴ ᴏᴛʜᴇʀꜱ ᴏᴘᴛɪᴏɴꜱ ʏᴏᴜ ᴄᴀɴ ᴇᴀʀɴ ʙʏ ʙᴇᴛᴛɪɴɢ.", reply_markup=all_platform)
     
-@Mukesh.on_message(filters.regex('📍ᴄᴀɴᴄᴇʟ ʙᴇᴛ📍') & filters.private)
+@JN.on_message(filters.regex('📍ᴄᴀɴᴄᴇʟ ʙᴇᴛ📍') & filters.private)
 async def must_join_channel(bot: Client, msg):
     await msg.reply("**ᴘʟᴇᴀꜱᴇ ꜱᴇʟᴇᴄᴛ ᴏᴘᴛɪᴏɴ:**\n\nɪɴ ʀᴇꜰᴇʀ ᴀɴᴅ ᴇᴀʀɴ ʏᴏᴜ ᴄᴀɴ ᴇᴀʀɴ ʙʏ ɪɴᴠɪᴛᴇ ᴜꜱᴇʀ\n\nɪɴ ᴏᴛʜᴇʀꜱ ᴏᴘᴛɪᴏɴꜱ ʏᴏᴜ ᴄᴀɴ ᴇᴀʀɴ ʙʏ ʙᴇᴛᴛɪɴɢ.", reply_markup=all_platform)
