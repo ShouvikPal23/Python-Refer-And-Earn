11604aea5490e204bbd89c8be7219d429e5cbe2c
Narayanpurohit <88087099+Narayanpurohit@users.noreply.github.com>
2024-08-01 13:33:53 +0530

    Update callback.py

--- Plugins/modules/callback.py
+++ Plugins/modules/callback.py
@@ -10,6 +10,39 @@ from pyrogram.enums import ChatMemberStatus
 @Client.on_callback_query()
 async def callback_all(client, query: CallbackQuery):
     
+    import re
+
+if query.data.startswith("approve"):
+    datas = query.data.split("_")
+    user_id = int(datas[1])
+    amount = float(datas[2])
+    upi_id = datas[4]
+    
+    print("Approve action", user_id, amount, upi_id)
+    
+    await JN.send_message(
+        user_id,
+        f"Your withdrawal request for INR {amount} has been approved."
+    )
+    await query.answer("Withdrawal approved.")
+
+elif query.data.startswith("reject"):
+    datas = query.data.split("_")
+    user_id = int(datas[1])
+    amount = float(datas[2])
+    upi_id = datas[4]
+    
+    print("Reject action", user_id, amount, upi_id)
+    
+    collection.update_one({'user_id': user_id}, {'$inc': {'balance': amount}})
+    await JN.send_message(
+        user_id,
+        f"Your withdrawal request for INR {amount} has been rejected. The amount has been refunded to your balance."
+    )
+    await query.answer("Withdrawal rejected.")
+
+    
+    
     match = re.match(r"^(approve|reject)_(\d+)_(\d+(\.\d+)?)_(.+)$", query.data)
     # print("33",match)
     if match:
